c1eec195166791847ded9e73d1fecb9a486fe4e3
levytsroman <r.levyts@gmail.com>
2016-12-06 20:31:26 -0500

    users can now downvote only logged in users can vote

--- app/controllers/question_vote.rb
+++ app/controllers/question_vote.rb
@@ -7,30 +7,18 @@
 
 
 post '/questions/:question_id/votes' do
-  question = Question.find(params[:question_id])
-  user = User.find(session[:id])
-  #
-  new_vote = Vote.new(user_id: user.id, votable: question, up_down: true)
-  if new_vote.save && request.xhr?
-    vote_num(question)
-
-    content_type :json
-    {vote_num: vote_num(question), question_id: question.id}.to_json
+  if session[:id]
+    question = Question.find_by(id: params[:question_id])
+    user = User.find_by(id: session[:id])
+    params[:up_down] == 'up' ? user_vote = true : user_vote = false
+    new_vote = Vote.new(user_id: user.id, votable: question, up_down: user_vote)
+    if new_vote.save && request.xhr?
+      vote_num(question)
+    end
   end
+  content_type :json
+  {vote_num: vote_num(question), question_id: question.id}.to_json
 end
-
-# get '/questions/:question_id/votes/:id' do
-#   @question = Question.find(params[:question_id])
-#   @vote = @question.votes.find(params[:id])
-#   erb :'votes/show'
-# end
-#
-# get '/questions/:question_id/votes/:id/edit' do
-#   @question = Question.find(params[:question_id])
-#   @vote = @question.votes.find(params[:id])
-#   erb :'votes/edit'
-# end
-
 
 put '/questions/:question_id/votes/:id' do
   @question = Question.find(params[:question_id])
--- public/js/votes.js
+++ public/js/votes.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
-  $('.votes').on('click', '.vote_up', function(event){
+  $('.votes').on('click', '.vote', function(event){
     event.preventDefault();
+
     var route = $(this).attr('href'),
+        direction = $(this).attr('id'),
         currentQuestion = $(this).parent().parent();
     $.ajax({
       url: route,
-      method: 'post'
+      method: 'post',
+      data: {up_down: direction}
     }).done(function(response){
       currentQuestion.find('.vote_number').html(response.vote_num)
     })
