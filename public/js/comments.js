1c8437d14e7aa7645bb38a479965a83bad841264
Wellington Vicioso <wellington.vicioso@gmail.com>
2016-12-07 16:47:02 -0500

    fix appenening of comments

--- public/js/comments.js
+++ public/js/comments.js
@@ -16,13 +16,14 @@ $(document).ready(function () {
 
   $('.add_comments').on('click', function(event) {
     event.preventDefault();
-    $(event.target).parent().hide();
-    $(event.target).parent().next().show();
+    $(event.target).hide();
+    $(event.target).next().next().next().show();
   });
 
   $('.cancel').on('click', function(event) {
     event.preventDefault();
     $(event.target).parent().hide();
+    $(event.target).parent().prev().prev().prev().show();
   });
 
   $(".create_comment").submit(function(event) {
@@ -32,8 +33,12 @@ $(document).ready(function () {
       url: route,
       type: "POST",
       data: $(this).serialize()
-    }).done(function(new_comment) {
-    $(event.target).parent().next().children().first().append("<li class='important'>" + new_comment.text + "</li><br><li class='imp'>" + new_comment.user + "</li><br>")
+    }).done(function(response) {
+      $(event.target).parent().next().show()
+      $(event.target).hide()
+      $(event.target).parent().next().append("<div class='answer-vote'><p id='answer.id>' class='answer-body grey lighten-4'>" + response.comment + "</p></div><div class='answer-comment left-align'><div class='answer-date'><a class='answer-user' href='/users/answer.user.id'>" + response.user + "</a>" + response.date + "</div></div>")
+
+
     });
   });
 
