6d79977fd8477304d8bba3b0b89f6e9f34f91d6d
levytsroman <r.levyts@gmail.com>
2016-12-10 05:50:59 -0500

    iron out some bugs in vote logic

--- public/js/votes.js
+++ public/js/votes.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
   $('.votes').on('click', '.vote', function(event){
     event.preventDefault();
-
     var route = $(this).attr('href'),
+        $linkClass = $(this).attr('class'),
         direction = $(this).attr('id'),
         currentQuestion = $(this).parent().parent();
     $.ajax({
@@ -11,11 +11,11 @@ $(document).ready(function() {
       method: 'post',
       data: {up_down: direction}
     }).done(function(response){
-      if (response.vote_class === 'voted_up'){
+      if (response.vote_class === 'voted_up' &&  $linkClass.indexOf('up') === -1){
         currentQuestion.find('.voted_down').removeClass('voted_down');
         currentQuestion.find('#up').attr('class', 'vote ' + response.vote_class)
         currentQuestion.find('.vote_number').html(response.vote_num - 1)
-      } else {
+      } else if(response.vote_class === 'voted_down' && $linkClass.indexOf('down') === -1){
         currentQuestion.find('.voted_up').removeClass('voted_up');
         currentQuestion.find('#down').attr('class', 'vote ' + response.vote_class)
         currentQuestion.find('.vote_number').html(response.vote_num + 1)
